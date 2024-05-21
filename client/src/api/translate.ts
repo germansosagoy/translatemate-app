a7496c72cb5d4fdbd9c8bfcb6420069b0ca60493
germansosagoy <germanesosagoy@gmail.com>
2024-05-21 17:52:10 -0300

    refactor: update API translate.ts

--- client/src/api/translate.ts
+++ client/src/api/translate.ts
@@ -1,13 +1,59 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/translate';
-
-export const translate = async (text: string) => {
-    try {
-        const response = await axios.post(API_URL, {text});
-        return response.data;
-    } catch (error) {
-        console.error('Error al traducir el text:', error);
-        throw error;
-    }
+export interface Language {
+  name: string;
+  nativeName: string;
+  dir: string;
 }
+
+export const translateText = async (inputText: string, outputLang: string) => {
+  const options = {
+    method: 'POST',
+    url: 'https://microsoft-translator-text.p.rapidapi.com/translate',
+    params: {
+      'to[0]': outputLang,
+      'api-version': '3.0',
+      profanityAction: 'NoAction',
+      textType: 'plain'
+    },
+    headers: {
+      'content-type': 'application/json',
+      'X-RapidAPI-Key': import.meta.env.VITE_RapidAPI_KEY,
+      'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+    },
+    data: [
+      {
+        Text: inputText
+      }
+    ]
+  };
+
+  try {
+    const response = await axios.request(options);
+    return response.data[0]?.translations[0]?.text;
+  } catch (error) {
+    console.error('Error traduciendo el texto:', error);
+    throw error;
+  }
+};
+
+export const getAvailableLanguages = async () => {
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://microsoft-translator-text.p.rapidapi.com/languages',
+      params: {
+        'api-version': '3.0'
+      },
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RapidAPI_KEY,
+        'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com'
+      }
+    };
+    const response = await axios.request(options);
+    return response.data.translation;
+  } catch (error) {
+    console.error('Error obteniendo los idiomas disponibles:', error);
+    throw error;
+  }
+};
